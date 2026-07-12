c7a2c3ae2288943cd6ab6cf53c8d192b2cf33bd6
坂本大幹 <saka.ttt.1219@gmail.com>
2026-07-12 17:35:07 +0900

    Initial commit: Add backend, frontend, and docker-compose

---
+++ backend/src/test/java/enquete_backend/EnqueteBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package enquete_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnqueteBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
